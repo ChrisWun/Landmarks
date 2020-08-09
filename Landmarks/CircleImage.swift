6ec74e219e804789da2350af845c277c3001856b
Christian Wunder <c.wunder@opwoco.com>
2020-08-09 12:49:30 +0200

    Section 4: Custom Image View

---
+++ Landmarks/CircleImage.swift
@@ -0,0 +1,24 @@
+//
+//  CircleImage.swift
+//  Landmarks
+//
+//  Created by Christian Wunder on 09.08.20.
+//  Copyright © 2020 Christian Wunder. All rights reserved.
+//
+
+import SwiftUI
+
+struct CircleImage: View {
+    var body: some View {
+        Image("turtlerock")
+        .clipShape(Circle())
+            .overlay(Circle().stroke(Color.white, lineWidth: 4))
+        .shadow(radius: 10)
+    }
+}
+
+struct CircleImage_Previews: PreviewProvider {
+    static var previews: some View {
+        CircleImage()
+    }
+}
